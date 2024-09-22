877394c04ecb36dd20c431c6c2c5e841d9822a49
Zach Sevart <143130706+zachsevart@users.noreply.github.com>
2024-09-22 18:26:12 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import requests
+import streamlit as st
 
 
 season_id = "2024"
@@ -21,21 +22,85 @@ headers = {
     'x-fantasy-source' : 'kona'
 }
 
-r = requests.get(url, headers=headers, cookies = espn_cookies)
-rTeam = requests.get(teamUrl, headers=headers, cookies = espn_cookies)
 
-player_data = r.json()
-team_data = rTeam.json()
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
-print(team_data)
 
-df = pd.DataFrame(player_data)
-player_df = df[['defaultPositionId', 'fullName', 'id', 'proTeamId']].copy()
-player_df.rename(columns= {'id':'player_id'}, inplace = True)
-print(player_df.sample(5))
+
+def get_draft_details(league_id, season_id):
+
+    url = "https://lm-api-reads.fantasy.espn.com/apis/v3/games/ffl/seasons/{}/segments/0/leagues/{}?view=mDraftDetail&view=mSettings&view=mTeam&view=modular&view=mNav".format(season_id, league_id)
+
+    r = requests.get(url, headers=headers, cookies = espn_cookies)
+
+    raw = r.json()
+    draftDetails = raw[0]
+    picks = draftDetails['draftDetail']['picks']
+    df = pd.DataFrame(picks)
+    draft_df = df[['overallPickNumber', 'playerId', 'teamId']].copy()
+    return draft_df
+
+def get_player_info(season_id):
+    url = "https://lm-api-reads.fantasy.espn.com/apis/v3/games/ffl/seasons/{}/players?scoringPeriodId=0&view=players_wl".format(season_id)
+
+    r = requests.get(url, headers=headers, cookies = espn_cookies)
+
+    raw = r.json()
+    players = raw[0]
+
+    df = pd.DataFrame(players)
+
+
+    player_df = df[['defaultPositionId', 'fullName', 'id', 'proTeamId']].copy()
+    player_df.rename(columns= {'id':'player_id'}, inplace = True)
+
+    return player_df
+
+def get_team_info(season_id):
+    url = "https://lm-api-reads.fantasy.espn.com/apis/v3/games/ffl/seasons/{}?view=proTeamSchedules_wl".format(season_id)
+
+    r = requests.get(url, headers=headers, cookies = espn_cookies)
+
+    raw = r.json()
+
+    team_names = raw['settings']['proTeams']
+
+    dfteam = pd.DataFrame(team_names)
+    teamdf = dfteam[['id', 'location', 'name']].copy()
+    teamdf["team name"] = teamdf['location'].astype(str) + ' '+ teamdf['name']
+    teamdf.rename(columns = {'id': 'team_id'}, inplace = True)
+
+
+
+st.title("Fantasy Football Data Viewer")
+
+# Inputs for league_id and season_id
+league_id_input = st.text_input("Enter League ID", value=league_id)
+season_id_input = st.text_input("Enter Season ID", value=season_id)
+
+# Select which data to view
+option = st.selectbox(
+    'Which data would you like to view?',
+    ('Draft Details', 'Player Info', 'Team Info')
+)
+
+if st.button("Fetch Data"):
+    if option == 'Draft Details':
+        draft_data = get_draft_details(league_id_input, season_id_input)
+        st.write("Draft Details Data")
+        st.dataframe(draft_data)
+    elif option == 'Player Info':
+        player_data = get_player_info(season_id_input)
+        st.write("Player Info Data")
+        st.dataframe(player_data)
+    elif option == 'Team Info':
+        team_data = get_team_info(season_id_input)
+        st.write("Team Info Data")
+        st.dataframe(team_data)
+
+
 
 
 
